8106058115cb4ffe41e9118a3a087c6bf87d715d
Maximiliano Garcia <maxig8@hotmail.com>
2021-03-27 13:49:51 +0100

    Fix index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
-import * as React from 'react';
-import Menu from '../components/templates/Menu';
+import { Link } from 'gatsby';
+import React from 'react';
+import Layout from '../components/templates/Layout';
 const IndexPage = () => {
-    return <Menu>Hola</Menu>;
+    return (
+        <Layout>
+            <h1> Cartas: </h1>
+            <ul>
+                <li>
+                    <Link to='/carta-del-comedor'>Carta del Comedor</Link>
+                </li>
+                <li>
+                    <Link to='/carta-de-terraza'>Carta de Terraza</Link>
+                </li>
+            </ul>
+        </Layout>
+    );
 };
 
 export default IndexPage;
