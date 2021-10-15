465deb3778929fecdd2de29aec690147c5e781b4
Maximiliano Garcia Mortigliengo <38573357+MaxiGarcia13@users.noreply.github.com>
2021-10-16 01:42:54 +0200

    Change menu

--- src/components/molecules/MenuItem/MenuItem.component.jsx
+++ src/components/molecules/MenuItem/MenuItem.component.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import './menuItem.scss';
+import React from "react";
+import "./menuItem.scss";
 
 const MenuItemComponent = ({ title, heroTitle, description, data }) => (
-    <section className='menu__section'>
-        {heroTitle && <h3 className='menu__section__heroTitle'>{heroTitle}</h3>}
-        {title !== null && <h2>{title}</h2>}
-        {description !== null && <p className='description'>{description}</p>}
-        <table className='menu__section__table'>
-            <tbody>
-                {data.map((item) => (
-                    <tr key={item.title} className='row'>
-                        <td className='text'>
-                            <div> {item.title}</div>
-                            {item.description && <div className='description'>{item.description}</div>}
-                        </td>
-                        <td className='price'>{item.price}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    </section>
+  <section className="menu__section">
+    {heroTitle && <h3 className="menu__section__heroTitle">{heroTitle}</h3>}
+    {title !== null && <h2>{title}</h2>}
+    {description !== null && <p className="description">{description}</p>}
+    <table className="menu__section__table">
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.title} className="row">
+            <td className="text">
+              <div> {item.title}</div>
+              {item.description && (
+                <div className="description">{item.description}</div>
+              )}
+              {item.lists && (
+                <ul>
+                  {item.lists.map((itemList) => {
+                    return (
+                      <li className="menu__section_list_item">{itemList}</li>
+                    );
+                  })}
+                </ul>
+              )}
+            </td>
+            <td className="price">{item.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </section>
 );
 
 export default MenuItemComponent;
